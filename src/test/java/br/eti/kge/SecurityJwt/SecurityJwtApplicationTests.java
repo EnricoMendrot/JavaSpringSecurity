a2e8d2791d535e78d220f1e782178ecf27830c94
EnricoMendrot <enricomendrot23@gmail.com>
2025-08-01 14:01:25 -0300

    Inicio do Projeto com Spring Boot Security

---
+++ src/test/java/br/eti/kge/SecurityJwt/SecurityJwtApplicationTests.java
@@ -0,0 +1,14 @@
+package br.eti.kge.SecurityJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+            System.out.println("\n\n\n*** Teste carregado! ***\n\n\n");
+	}
+
+}
